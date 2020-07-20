15b8a8f974d121e01e1928881543a677c374256f
13circle <13circle97@gmail.com>
2020-07-20 23:00:29 +0900

    My 2nd Django Blog

---
+++ blogproject/blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 200)
+    writer = models.CharField(max_length = 50, null = True)
+    pub_date = models.DateTimeField('date published')
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
